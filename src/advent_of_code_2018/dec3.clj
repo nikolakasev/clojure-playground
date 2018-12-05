b50ae47b4aabe4966e0b030b49a674046d3bc26f
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-05 20:33:40 +0100

    solved D3P1

--- src/advent_of_code_2018/dec3.clj
+++ src/advent_of_code_2018/dec3.clj
@@ -1,10 +1,13 @@
-(ns advent-of-code-2018.dec3)
+(ns advent-of-code-2018.dec3
+  (:require [clojure.math.combinatorics :as combo])
+  (:require [clojure.math.numeric-tower :as math]))
 
 (def id "#1 @ 1,3: 4x4")
 (def id-2 "#2 @ 3,1: 4x4")
 (def id-3 "#3 @ 5,5: 2x2")
 
 (re-matches #"#\d*\s@\s(\d*)\,(\d*):\s(\d*)x(\d*)" id)
+(re-matches #"#\d*\s@\s(\d*)\,(\d*):\s(\d*)x(\d*)" "#13 @ 68,980: 11x19")
 
 (System/getProperty "user.dir")
 (slurp "src/advent_of_code_2018/input-dec3.txt")
@@ -12,22 +15,76 @@
 (defn box-coordinates
   [box-id]
   (let [groups (rest (re-matches #"#\d*\s@\s(\d*)\,(\d*):\s(\d*)x(\d*)" box-id))
-        r (read-string (second groups))
-        c (read-string (first groups))
-        wide (read-string (first (reverse groups)))
-        tall (read-string (second (reverse groups)))]
+        c (read-string (second groups))
+        r (read-string (first groups))
+        tall (read-string (first (reverse groups)))
+        wide (read-string (second (reverse groups)))]
     [[r c] [r (+ c wide)] [(+ r tall) c] [(+ r tall) (+ c wide)]]))
+
+(defn fabric-span
+  [claim-id]
+  (let [groups (rest (re-matches #"#\d*\s@\s(\d*)\,(\d*):\s(\d*)x(\d*)" claim-id))
+        x (read-string (first groups))
+        y (read-string (second groups))
+        tall (read-string (first (reverse groups)))
+        wide (read-string (second (reverse groups)))]
+    [x y wide tall]))
+
+(box-coordinates "#13 @ 68,980: 11x19")
+(fabric-span "#13 @ 68,980: 11x19")
 
 (box-coordinates id)
 (box-coordinates id-2)
+(nth (box-coordinates id-3) 0)
 
-(defn overlap?
-  [box-one
-   box-two]
-  false)
+(>= 2 4)
 
-(interleave [1 2] [4 5])
+(math/abs -2)
 
-(= true (overlap? id id-2))
-(= false (overlap? id id-3))
-(= false (overlap? id-2 id-3))
+(defn overlapping-area
+  [ur1
+   ll1
+   ur2
+   ll2]
+  (* (max 0 (- (min (first ll1) (first ll2))
+               (max (first ur1) (first ur2))))
+     (max 0 (- (min (second ur1) (second ur2))
+               (max (second ll1) (second ll2))))))
+
+(overlapping-area (second (box-coordinates id)) (nth (box-coordinates id) 2) (second (box-coordinates id-2)) (nth (box-coordinates id-2) 2))
+
+(overlapping-area (second (box-coordinates id-2)) (nth (box-coordinates id-2) 2) (second (box-coordinates id-3)) (nth (box-coordinates id-3) 2))
+
+(min 5 0)
+
+;the overlap between box 2 and 3 is 4 which is correct
+(apply + (map (fn [[one two]] (overlapping-area (second (box-coordinates one)) (nth (box-coordinates one) 2) (second (box-coordinates two)) (nth (box-coordinates two) 2))) (combo/combinations [id id-2] 2)))
+
+;understood the puzzle wrong... calculates an overlap for each unique pair and sum up for a total of 186644 which is too high...
+(apply + (map (fn [[one two]] (overlapping-area (second (box-coordinates one)) (nth (box-coordinates one) 2) (second (box-coordinates two)) (nth (box-coordinates two) 2))) (combo/combinations (clojure.string/split-lines (slurp "src/advent_of_code_2018/input-dec3.txt")) 2)))
+
+;alternative of (frequencies list)
+(count (filter (fn [[_ list]] (> (count list) 1)) (group-by identity [[1 2] [1 2] [3 4] [5 6] [1 2] [5 6] [5 7]])))
+
+(identity [1 2])
+
+(defn area-of-fabric
+  [claim]
+  (let [[b-x b-y wide tall] (fabric-span claim)]
+    (for [x (range b-x (+ b-x wide))
+          y (range b-y (+ b-y tall))]
+      [x y])))
+
+(mapv #(+ 1 %) (map (fn [y] [3, y]) (range 3 6)))
+
+(area-of-fabric id)
+
+(def input-small "#1 @ 387,801: 11x22
+#2 @ 101,301: 19x14
+#3 @ 472,755: 11x16")
+
+(def input
+  (slurp "src/advent_of_code_2018/input-dec3.txt"))
+
+;115304 - solved D3P1
+(count (filter (fn [[_ list]] (> (count list) 1)) (group-by identity (mapcat area-of-fabric (clojure.string/split-lines input)))))
