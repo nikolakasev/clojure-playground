98e2b4f8102a8357f53982b67d0d1947b5ec152e
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-14 08:41:44 +0100

    input to data-structure

--- src/advent_of_code_2018/dec12.clj
+++ src/advent_of_code_2018/dec12.clj
@@ -30,3 +30,20 @@
 (str/index-of input-small "..#.#..##")
 
 (re-matches #"([\.#]*)\s=>\s([\.#])" "...## => #")
+
+(let [m (re-matcher #"a" "bbbabbb")] (.find m) (.start m))
+
+(let [m (re-matcher #"#." input-small)] (.find m 6) (.start m))
+
+(defn generation
+  [input]
+  (loop [characters (vec input)
+         so-far #{}
+         position 0]
+    (if (empty? characters)
+      so-far
+      (if (= \# (first characters))
+        (recur (rest characters) (into so-far [position]) (inc position))
+        (recur (rest characters) so-far (inc position))))))
+
+(generation input-small)
