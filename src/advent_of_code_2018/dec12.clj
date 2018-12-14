9f65aca82f3a4a9e3209f9769ba1575f43a2a45a
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-14 22:32:19 +0100

    data-structure works

--- src/advent_of_code_2018/dec12.clj
+++ src/advent_of_code_2018/dec12.clj
@@ -1,12 +1,14 @@
 (ns advent-of-code-2018.dec12
-  (:require [clojure.string :as str])
-  (:require [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
 (def input-small
   "#..#.#..##......###...###")
 
+(def instruction-regex
+  #"([\.#]*)\s=>\s([\.#])")
+
 (def instructions
-  (str/split-lines "...## => #
+  (map (fn [[_ instruction plant-or-not]] [instruction plant-or-not]) (map (partial re-matches instruction-regex) (str/split-lines "...## => #
 ..#.. => #
 .#... => #
 .#.#. => #
@@ -19,7 +21,7 @@
 ##.## => #
 ###.. => #
 ###.# => #
-####. => #"))
+####. => #"))))
 
 (re-matches #"(\.\.##\.\.)" input-small)
 
@@ -29,21 +31,45 @@
 
 (str/index-of input-small "..#.#..##")
 
-(re-matches #"([\.#]*)\s=>\s([\.#])" "...## => #")
+(re-matches instruction-regex "...## => #")
 
 (let [m (re-matcher #"a" "bbbabbb")] (.find m) (.start m))
 
 (let [m (re-matcher #"#." input-small)] (.find m 6) (.start m))
 
-(defn generation
-  [input]
+(defn string-to-generation
+  [input
+   begins-at-pot]
   (loop [characters (vec input)
          so-far #{}
-         position 0]
+         position begins-at-pot]
     (if (empty? characters)
       so-far
       (if (= \# (first characters))
         (recur (rest characters) (into so-far [position]) (inc position))
         (recur (rest characters) so-far (inc position))))))
 
-(generation input-small)
+(apply max (string-to-generation input-small 0))
+
+(range 1 10)
+
+(defn generation-to-string
+  [generation
+   begins-at-pot]
+  (let [max-pot-on-the-right (apply max generation)]
+    (str/join (map #(if (contains? generation %) "#" ".") (range begins-at-pot (+ 1 max-pot-on-the-right))))))
+
+;input to structure and back works
+(= (generation-to-string (string-to-generation input-small 0) 0) input-small)
+
+;structure works for end result
+(= 325 (apply + (string-to-generation "#....##....#####...#######....#.#..##." -2)))
+
+(defn generations
+  [generation
+   instructions
+   number-of-generations-wanted]
+  [0 0])
+
+;algo should work on the example
+(= 325 (apply + (generations (string-to-generation "#..#.#..##......###...###." 0) instructions 20)))
