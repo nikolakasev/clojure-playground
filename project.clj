4167c33bed5b187f886f8c93fdcc22452e5d72f2
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-15 15:58:40 +0100

    loom is a graph library

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [org.clojure/math.combinatorics "0.1.4"]
-                           [org.clojure/math.numeric-tower "0.0.4"]]
+                           [org.clojure/math.numeric-tower "0.0.4"]
+                           [aysylu/loom "1.0.2"]]
             :global-vars {*warn-on-reflection* true})
