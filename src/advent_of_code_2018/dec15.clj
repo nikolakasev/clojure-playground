fde6b7069229cf0321b99ff6d54ebb0171fdca58
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-16 14:43:47 +0100

    enemies left, atack, total health, reading order

--- src/advent_of_code_2018/dec15.clj
+++ src/advent_of_code_2018/dec15.clj
@@ -92,12 +92,14 @@
                      x (range 1 (- max-x 1))]
                  (let [character (nth (nth lines y) x)]
                    (if (and (not (= \# character)) (not (= \. character)))
-                     [(keyword (str character)) [x y] initial-health])))]
+                     {:type (str character) :location [x y] :hp initial-health})))]
     (filter (comp not nil?) actors)))
 
 (def actors (input-to-actors input 300))
 
-(concat [{:1 2}] [{:3 4}])
+(:E (first actors))
+
+(:1 (first (concat [{:1 2}] [{:3 4}])))
 
 ;builds the graph
 (def b (g/weighted-digraph (apply hash-map (mapcat identity (input-to-board input)))))
@@ -109,11 +111,75 @@
 
 (= \# \.)
 
-(keyword (str \G))
+(defn enemies-left
+  [actors]
+  (= 2 (count (group-by :type actors))))
+
+(enemies-left actors)
+
+(defn total-health
+  [actors]
+  (apply + (map :hp actors)))
+
+(total-health actors)
+
+(def sort-by-reading-order
+  (partial sort-by #(+ (first (:location %))
+                       (* 10 (second (:location %))))))
+
+(sort-by-reading-order actors)
+
+(group-by first actors)
 
 (defn battle
   [input]
-  0)
+  (let [board (g/weighted-digraph (apply hash-map (mapcat identity (input-to-board input))))
+        actors (sort-by-reading-order (input-to-actors input))]
+    (loop [round 0
+           ;changes only when an actor dies
+           all-actors actors
+           ;for each actor apply it's logic
+           actors-left actors]
+      (if (enemies-left all-actors)
+        ;continue round
+        (let [actor (first actors-left)
+              adjacent-enemies []
+              enemy-coordinates []
+              enemies-in-range []]
+          (if (not (empty? adjacent-enemies))))
+            ;attack the enemy with the lowest health
+            ;move
+
+        ;battle ends
+        (* (dec round) (total-health all-actors))))))
+
+(defn move-actor
+  "Moves an actor from one coordinate to another."
+  [[x y]
+   [new-x new-y]
+   all-actors])
+
+(move-actor)
+
+(defn attack-actor
+  [location
+   all-actors
+   f]
+  (map #(if (= location (:location %)) (update % :hp f) %) all-actors))
+
+(= [1 2] [1 4])
+
+(:location (first actors))
+
+(attack-actor [3 4] actors (fn [hp] (- hp 3)))
+
+({:type "G", :location [2 1], :hp 300} {:type "E", :location [4 2], :hp 300} {:type "G", :location [5 2], :hp 300} {:type "G", :location [5 3], :hp 300} {:type "G", :location [3 4], :hp 300} {:type "E", :location [5 4], :hp 300})
+
+(defn remove-dead-actors
+  [actors]
+  (filter #(> (:hp %) 0) actors))
+
+(not (empty? [1 2]))
 
 (= 27730 (battle "#######
 #.G...#
