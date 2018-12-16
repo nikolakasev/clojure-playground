4a71afc0cca8cdfaaaba995e395d50a67f229fcb
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-16 20:35:15 +0100

    infinite loop

--- src/advent_of_code_2018/dec15.clj
+++ src/advent_of_code_2018/dec15.clj
@@ -3,7 +3,18 @@
   (:require [loom.graph :as g])
   (:require [loom.io :as io])
   (:require [loom.alg :as a])
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set])
+  (:require [clojure.pprint :as prn]))
+
+(defmacro dlet
+  "let with inspected bindings"
+  [bindings & body]
+  `(let [~@(mapcat (fn [[n v]]
+                     (if (or (vector? n) (map? n))
+                       [n v]
+                       [n v '_ `(println (name '~n) ":" ~v)]))
+                   (partition 2 bindings))]
+     ~@body))
 
 (def g (g/graph [1 2] [2 3] {3 [4] 5 [6 7]} 7 8 9))
 
@@ -137,11 +148,18 @@
 
 (total-health actors)
 
-(def sort-by-reading-order
+;TODO re-factor with locations-by-reading-order?
+(def actors-by-reading-order
   (partial sort-by #(+ (first (:location %))
                        (* 10 (second (:location %))))))
 
-(sort-by-reading-order actors)
+(actors-by-reading-order actors)
+
+(def locations-by-reading-order
+  (partial sort-by #(+ (first %)
+                       (* 10 (second %)))))
+
+(locations-by-reading-order [[1 2] [1 1] [3 4]])
 
 (group-by first actors)
 
@@ -178,30 +196,38 @@
 
 (not (empty? [1 2]))
 
-(defn actor-enemies
+(defn enemies-of
   [actor
    all-actors]
   (filter #(not (= (:type actor) (:type %))) all-actors))
+
+(defn allies-of
+  [actor
+   all-actors]
+  ;same type, but not the same actor
+  (filter #(and (= (:type actor) (:type %)) (not (= (:location actor) (:location %)))) all-actors))
 
 (defn adjacent-enemies
   [actor
    all-actors
    board]
   (let [adjacent-locations (set (g/successors board (location-to-node (:location actor))))
-        enemy-locations (set (map location-to-node (map :location (actor-enemies actor all-actors))))
-        adjacent-enemies (map (fn [node] (filter #(= (node-to-location node) (:location %)) all-actors)) (set/intersection adjacent-locations enemy-locations))]
+        enemy-locations (set (map (comp location-to-node :location) (enemies-of actor all-actors)))
+        adjacent-enemies (mapcat (fn [node] (filter #(= (node-to-location node) (:location %)) all-actors)) (set/intersection adjacent-locations enemy-locations))]
     adjacent-enemies))
 
-(adjacent-enemies (nth actors 2) actors b)
+(adjacent-enemies (second actors) actors b)
 
-(set/intersection #{1 2 3} #{3 4 5})
+(prn/pprint (set/intersection #{1 2 3} #{3 4 5}))
 
-(actor-enemies (first actors) actors)
+(defn not-an-ally
+  [allies neighbor predecessor depth]
+  (not (contains? allies neighbor)))
 
 (defn battle
   [input]
   (let [board (g/weighted-digraph (apply hash-map (mapcat identity (input-to-board input))))
-        actors (sort-by-reading-order (input-to-actors input 300))
+        actors (actors-by-reading-order (input-to-actors input 300))
         attack-function #(- % 3)]
     (loop [round 0
            ;changes only when an actor dies
@@ -216,16 +242,30 @@
                  all-actors)
           ;round continues
           (let [actor (first actors-left)
+                ;to be able to attack
                 adjacent-enemies (adjacent-enemies actor all-actors board)
-                enemy-coordinates []
-                enemies-in-range []]
+                ;TODO refactor next two and "inject" enemies-of or allies-of?
+                enemy-nodes (set (map (comp location-to-node :location) (enemies-of actor all-actors)))
+                allied-nodes (set (map (comp location-to-node :location) (allies-of actor all-actors)))
+                reachable-enemies (set/intersection (set (a/bf-traverse board (:location actor) :when (partial not-an-ally allied-nodes)))
+                                                    enemy-nodes)]
             (if (not (empty? adjacent-enemies))
               ;attack! the enemy with the lowest health and clean up the mess
               (recur round
                      (remove-dead-actors (attack-actor (first (sort-by :hp adjacent-enemies)) all-actors attack-function))
-                     (rest actors-left)))))
-              ;move
-
+                     (rest actors-left))
+              (if (not (empty? reachable-enemies))
+                ;move if there are reachable enemies
+                (let [in-range (set/difference (set (mapcat #(g/successors board %) reachable-enemies))
+                                               (set (map (comp location-to-node :location) actors)))
+                      move-to (first (locations-by-reading-order in-range))]
+                  (recur round
+                         (move-actor (:location actor) move-to all-actors)
+                         (rest actors-left)))
+                ;nothing to do because there are no adjacent or reachable enemies
+                (recur round
+                       all-actors
+                       (rest actors-left))))))
         ;battle ends
         (* (dec round) (total-health all-actors))))))
 
