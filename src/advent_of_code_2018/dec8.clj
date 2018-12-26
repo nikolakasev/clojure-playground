2cf4f18209be644cdb2b607f211614e0d0af8f2e
Nikola Kasev <nikola.kasev@gmail.com>
2018-12-26 15:47:04 +0100

    DFS logic

--- src/advent_of_code_2018/dec8.clj
+++ src/advent_of_code_2018/dec8.clj
@@ -28,7 +28,7 @@
 
 (mark-as-read history)
 
-(mark-as-read [[1 3]])
+(mark-as-read [[0 3] [1 2]])
 
 (defn get-meta
   [license]
@@ -49,3 +49,34 @@
 
 ;36027, solved P1
 (get-meta input)
+
+(def graph {:A [:B :C]
+            :B [:A]
+            :C [:A :D]})
+
+(defn visited?
+  "Predicate which returns true if the node v has been visited already, false otherwise."
+  [v coll]
+  (some #(= % v) coll))
+
+(defn find-neighbors
+  "Returns the sequence of neighbors for the given node"
+  [v coll]
+  (get coll v))
+
+(defn graph-dfs
+  "Traverses a graph in Depth First Search (DFS)"
+  [graph v]
+  (loop [stack   (vector v) ;; Use a stack to store nodes we need to explore
+         visited []]        ;; A vector to store the sequence of visited nodes
+    (if (empty? stack)      ;; Base case - return visited nodes if the stack is empty
+      visited
+      (let [v           (peek stack)
+            neighbors   (find-neighbors v graph)
+            not-visited (filter (complement #(visited? % visited)) neighbors)
+            new-stack   (into (pop stack) not-visited)]
+        (if (visited? v visited)
+          (recur new-stack visited)
+          (recur new-stack (conj visited v)))))))
+
+(graph-dfs graph :A)
