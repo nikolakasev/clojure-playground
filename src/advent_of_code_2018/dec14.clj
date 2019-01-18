fbda08edd910615786fcc77eca50fb86ee96f7b2
Nikola Kasev <nikola.kasev@gmail.com>
2019-01-18 16:31:50 +0100

    works on sample, but StackOverflow on input

--- src/advent_of_code_2018/dec14.clj
+++ src/advent_of_code_2018/dec14.clj
@@ -20,20 +20,24 @@
 
 (defn chocolate-recipe
   ([] (chocolate-recipe 0 1 2 [3 7]))
-  ([one two length so-far] (let [digits (digits (+ (nth so-far (mod one length))
-                                                   (nth so-far (mod two length))))]
-                             (lazy-cat so-far
-                                       digits
-                                       (chocolate-recipe (inc one)
-                                                         (inc two)
-                                                         (+ length (count digits))
+  ([one two length so-far] (let [score-one (nth so-far one)
+                                 score-two (nth so-far two)
+                                 digits (digits (+ score-one score-two))
+                                 length' (+ length (count digits))]
+                             (lazy-cat digits
+                                       (chocolate-recipe (mod (+ one score-one 1) length')
+                                                         (mod (+ two score-two 1) length')
+                                                         length'
                                                          (concat so-far digits))))))
 
-(nth [3 7 1 0] (mod 5 4))
+(nth [3 7 1 0 1 0] (mod (+ 1 7 1) 6))
 
-(take 10 (chocolate-recipe))
+(take 25 (chocolate-recipe))
+
+(take 10 (drop (- 286051 2) (chocolate-recipe)))
 
 (digits 8)
 
-(= [0 1 2 4 5 1 5 8 9 1] (take 10 (drop 5 (chocolate-recipe [3 7] 1 2 2))))
-(= [9 2 5 1 0 7 1 0 8 5] (take 10 (drop 2018 (chocolate-recipe [3 7] 1 2 2))))
+(= [0 1 2 4 5 1 5 8 9 1] (take 10 (drop 3 (chocolate-recipe))))
+(= [9 2 5 1 0 7 1 0 8 5] (take 10 (drop 16 (chocolate-recipe))))
+(= [5 9 4 1 4 2 9 8 8 2] (take 10 (drop 2016 (chocolate-recipe))))
