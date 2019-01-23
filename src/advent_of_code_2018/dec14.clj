529ebafeb598e9cd481c72925fac8be90779cee3
Nikola Kasev <nikola.kasev@gmail.com>
2019-01-23 14:42:03 +0100

    solved P2D14

--- src/advent_of_code_2018/dec14.clj
+++ src/advent_of_code_2018/dec14.clj
@@ -37,8 +37,33 @@
 ;solves P1
 (take 10 (drop (- 286051 2) (chocolate-recipe)))
 
+(first (chocolate-recipe))
+
 (digits 8)
 
 (= [0 1 2 4 5 1 5 8 9 1] (take 10 (drop 3 (chocolate-recipe))))
 (= [9 2 5 1 0 7 1 0 8 5] (take 10 (drop 16 (chocolate-recipe))))
 (= [5 9 4 1 4 2 9 8 8 2] (take 10 (drop 2016 (chocolate-recipe))))
+
+(defn choco-search
+  [scores]
+  (loop [recipes-so-far (chocolate-recipe)
+         scores-so-far scores
+         last-op-was-match false
+         acc 0]
+    (if (empty? scores-so-far)
+      ;2 because chocolate-recipe doesn't return the first two elements '(3 7)
+      (+ acc 2)
+      (if (= (first recipes-so-far) (first scores-so-far))
+        (recur (rest recipes-so-far) (rest scores-so-far) true acc)
+        (if last-op-was-match
+          (recur recipes-so-far scores false (+ (- (count scores) (count scores-so-far)) acc))
+          (recur (rest recipes-so-far) scores false (inc acc)))))))
+
+(= 9 (choco-search '(5 1 5 8 9)))
+(= 5 (choco-search '(0 1 2 4 5)))
+(= 18 (choco-search '(9 2 5 1 0)))
+(= 2018 (choco-search '(5 9 4 1 4)))
+
+;solves P2"Elapsed time: 197489.438892 msecs"
+;(time (choco-search '(2 8 6 0 5 1)))
